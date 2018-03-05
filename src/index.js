935beb8a0d0ab411f879ee6b003f2ca4c990c523
Chris Gillis <thechrisgillis@gmail.com>
2018-03-05 16:38:20 +0000

    Setup ecosystem

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+// Step 1: Method takes a pattern name
+// Step 2: Look for relevant files and load them (or return an empty string)
+// Step 3: Parse handlebars with provided parameters into html and return it.
+// Step 4: Return scss and js as-is.
+// Step 5: Test implementation on the client.
+
+export default {
+  double: (a) => {
+    return a*2;
+  },
+};
