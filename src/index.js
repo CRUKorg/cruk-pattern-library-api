b866aec55f807088412ddabeaee89b10037b3680
Chris Gillis <thechrisgillis@gmail.com>
2018-03-06 11:04:27 +0000

    Load files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
+import fs from 'fs';
+import path from 'path';
+import Handlebars from 'handlebars';
 
-// Step 1: Method takes a pattern name
-// Step 2: Look for relevant files and load them (or return an empty string)
-// Step 3: Parse handlebars with provided parameters into html and return it.
-// Step 4: Return scss and js as-is.
-// Step 5: Test implementation on the client.
+const patternsDir = '../patterns';
+
+const findFile = (dir, filter) => {
+  const files = fs.readdirSync(dir);
+  for(let i=0;i<files.length;i++){
+    const filename = path.join(dir, files[i]);
+    const stat = fs.lstatSync(filename);
+    if (stat.isDirectory()) {
+      findFile(filename, filter); //recurse
+    }
+    else if (filename.indexOf(filter) >= 0) {
+      return filename;
+    }
+  }
+  return false;
+};
+
+const getFileContents = (filename) => {
+  const path = findFile(patternsDir, filename);
+  const fileContents = fs.readFileSync(path, 'utf-8');
+
+  if (!fileContents || typeof fileContents !== 'string') {
+    return '';
+  }
+
+  return fileContents;
+};
+
+const processTemplate = (source, data) => {
+  // @TODO: Precompilation is faster apparently?
+  const template = Handlebars.compile(source);
+  return template(data);
+};
 
 export default {
-  double: (a) => {
-    return a*2;
+  getPatternMarkup: (patternName, patternConfig) => {
+    const template = getFileContents(`${patternName}.html`);
+    return processTemplate(template, patternConfig);
+  },
+  getPatternStyle: (patternName) => {
+    return getFileContents(`${patternName}.scss`);
+  },
+  getPatternScript: (patternName) => {
+    return getFileContents(`${patternName}.js`);
   },
 };
