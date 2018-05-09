775c8f7d626ff3d679417f07c4856fbb52dbcb7a
Chris Gillis <thechrisgillis@gmail.com>
2018-05-09 15:09:53 +0100

    Style collapsible box

--- patterns/collapsible.jsx
+++ patterns/collapsible.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Collapsible = (props) => {
-  return (
-    <div className="cr-collapsible">
-      <h2>{props.title}</h2>
-      <p>{props.content}</p>
-    </div>
-  );
-};
+class Collapsible extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapsed: this.props.collapsed,
+    };
+  }
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+  render() {
+    // We prefer taking content as a JSX object, but will also accept a static html string.
+    let content = this.props.content;
+    if (!this.props.content && this.props.staticContent) {
+      content = <div dangerouslySetInnerHTML={{ __html: this.props.staticContent }}/>;
+    }
+    return (
+      <div className={`cr-collapsible ${this.state.collapsed ? 'cr-collapsible--collapsed' : 'cr-collapsible--expanded'}`}>
+        <h2 className="cr-collapsible__heading">
+          <button
+            className="cr-collapsible__heading-button"
+            aria-expanded={!this.state.collapsed}
+            onClick={this.toggleCollapsed}
+          >
+            {this.props.title}
+          </button>
+        </h2>
+        <div className="cr-collapsible__content">
+          {content}
+        </div>
+      </div>
+    );
+  }
+}
 
 Collapsible.propTypes = {
+  collapsed: PropTypes.bool,
+  content: PropTypes.object, // Content as a JSX object.
+  staticContent: PropTypes.string, // Content as an html string.
   title: PropTypes.string,
-  content: PropTypes.object,
 };
 
 export default Collapsible;
