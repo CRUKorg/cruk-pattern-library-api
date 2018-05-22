e511b34f6a1cd09ed9a4300ccd1b6ca518ea09a7
Chris Gillis <thechrisgillis@gmail.com>
2018-05-22 16:47:27 +0100

    Implement twig in PHP

--- src/Patterns.php
+++ src/Patterns.php
@@ -2,6 +2,23 @@
 
 namespace Cruk\CrukPatternLibraryApi;
 
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+
 class Patterns {
+
+  protected $twig;
+
+  public function __construct() {
+    $twigLoader = new Twig_Loader_Filesystem('../patterns');
+    $twigSettings = [
+      'autoescape' => 'html',
+    ];
+    $this->twig = new Twig_Environment($twigLoader, $twigSettings);
+  }
+
+  public function getMarkup(string $patternName, array $patternData): ?string {
+    return $this->twig->render($patternName . '.twig', $patternData);
+  }
 
 }
