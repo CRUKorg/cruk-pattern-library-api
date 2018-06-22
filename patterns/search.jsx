d2377a4ba51db6fb5973ee5c8ab40658e94713db
Chris Gillis <thechrisgillis@gmail.com>
2018-06-21 21:31:08 -0700

    Improve input group styles:

--- patterns/search.jsx
+++ patterns/search.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-function Search(props) {
-  const defaultSubmitCallback = e => e.preventDefault();
-  return (
-    <form role="search" onSubmit={props.submitCallback || defaultSubmitCallback}>
-      <div className="cr-input-group cr-search-input">
-        <label htmlFor={props.uniqueId} className="cr-search-input__label">{props.label}</label>
-        <input type="search" name="search" id={props.uniqueId} onChange={props.changeCallback} className="cr-input-group__input cr-search-input__input" placeholder={props.label} autoComplete="off" autoCorrect="off" />
-        <button type="submit" className="cr-input-group__button cr-search-input__button" aria-label="Submit your search">Search</button>
-      </div>
-    </form>
-  );
+class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { focused: false };
+  }
+
+  onFocus = () => {
+    this.setState({ focused: true });
+  };
+
+  onBlur = () => {
+    this.setState({ focused: false });
+  };
+
+  render() {
+    const defaultSubmitCallback = e => e.preventDefault();
+    return (
+      <form
+        role="search"
+        onSubmit={this.props.submitCallback || defaultSubmitCallback}
+        className={`cr-search-input${this.state.focused ? ' cr-search-input--focused' : ''}`}
+      >
+        <label htmlFor={this.props.uniqueId} className="cr-search-input__label">{this.props.label}</label>
+        <input
+          type="search"
+          name="search"
+          id={this.props.uniqueId}
+          onChange={this.props.changeCallback}
+          className="cr-search-input__input"
+          placeholder={this.props.label}
+          autoComplete="off"
+          autoCorrect="off"
+          onBlur={this.onBlur}
+          onFocus={this.onFocus}
+        />
+        <button
+          type="submit"
+          className="cr-search-input__button"
+          aria-label="Submit your search"
+        >
+          Search
+        </button>
+      </form>
+    );
+  }
 }
 
 Search.propTypes = {
