c305a7e090cc4e182e9bb5acd1d52ed499f86c58
Chris Gillis <thechrisgillis@gmail.com>
2018-06-21 20:39:31 -0700

    Add search pattern

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ export {default as CtaSecondary} from './patterns/cta.secondary';
 export {default as Divider} from './patterns/divider';
 export {default as HeaderPageTitle} from './patterns/header.page-title';
 export {default as Loader} from './patterns/loader';
+export {default as Search} from './patterns/search';
 
 export default {
   getPatternMarkup: (patternName, patternConfig) => {
@@ -30,6 +31,7 @@ export default {
       'header.page-title',
       'layout',
       'loader',
+      'search',
       'text',
     ];
   },
---
+++ patterns/search.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Search(props) {
+  const defaultSubmitCallback = e => e.preventDefault();
+  return (
+    <form role="search" onSubmit={props.submitCallback || defaultSubmitCallback}>
+      <div className="cr-input-group cr-search-input">
+        <label htmlFor={props.uniqueId} className="cr-search-input__label">{props.label}</label>
+        <input type="search" id={props.uniqueId} onChange={props.changeCallback} className="cr-input-group__input cr-search-input__input" placeholder={props.label} autoComplete="off" autoCorrect="off" />
+        <button type="submit" className="cr-input-group__button cr-search-input__button" aria-label="Submit your search">Search</button>
+      </div>
+    </form>
+  );
+}
+
+Search.propTypes = {
+  changeCallback: PropTypes.func,
+  label: PropTypes.string,
+  submitCallback: PropTypes.func,
+  uniqueId: PropTypes.string,
+};
+
+export default Search;
