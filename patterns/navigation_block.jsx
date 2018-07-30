9d1d9b241fa8864b0ce0322b8e3b37eaabe796f1
Jessie Wang <Jessie.Wang@cancer.org.uk>
2018-07-30 15:04:50 +0100

    CON-1286 minor update

--- patterns/navigation_block.jsx
+++ patterns/navigation_block.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 function NavigationBlock(props) {
   return (
     <a className="cr-navigation-block__cta" href={props.href}>
-      <div>
-        <h2 className="cr-navigation-block">{props.title}</h2>
+      <div className="cr-navigation-block">
+        <h2 className="cr-navigation-block__title">{props.title}</h2>
         <div
           className="cr-navigation-block__content"
           dangerouslySetInnerHTML={{ __html: props.text }}
