ddd2c8d5fcda2f77cd2d2877fc9827a789df891c
Jessie Wang <Jessie.Wang@cancer.org.uk>
2018-07-30 11:50:31 +0100

    CON-1286 nav block hotfix

--- patterns/navigation_block.jsx
+++ patterns/navigation_block.jsx
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 
 function NavigationBlock(props) {
   return (
-    <a className="cr-navigation-block__cta" href={props.text}>
+    <a className="cr-navigation-block__cta" href={props.href}>
       <div>
         <h2 className="cr-navigation-block">{props.title}</h2>
+        <div
+          className="cr-navigation-block__content"
+          dangerouslySetInnerHTML={{ __html: props.text }}
+        />
         <div className="cr-navigation-block__content">
           {props.text}
         </div>
