4b7a82d32fab753c100164ac01974a1863a68263
Jessie Wang <Jessie.Wang@cancer.org.uk>
2018-09-14 16:11:12 +0100

    add related links pattern

---
+++ patterns/related.links.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class RelatedLinks extends Component {
+  render() {
+    let content = this.props.content;
+    if (!this.props.content && this.props.staticContent) {
+      content = <div dangerouslySetInnerHTML={{ __html: this.props.staticContent }}/>;
+    }
+    return (
+      <div className="cr-related-links">
+        <div className="cr-related-links__bar" />
+        <div className="cr-related-links__content">
+          <h3 className="cr-related-links__heading">{this.props.title}</h3>
+          <div className="cr-related-links__body">
+            {content}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+RelatedLinks.propTypes = {
+  content: PropTypes.object,
+  staticContent: PropTypes.string,
+  title: PropTypes.string,
+};
+
+export default RelatedLinks;
