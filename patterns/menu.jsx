ea673ebd58d9192b2bbb147eade7a766b6c18a20
Jessie Wang <Jessie.Wang@cancer.org.uk>
2018-11-09 16:32:34 +0000

    menu jsx component

--- patterns/menu.jsx
+++ patterns/menu.jsx
@@ -1,53 +1,118 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import { Fetch } from 'react-data-fetching';
+import Search from './search';
 
-function MegaMenu(props) {
-  return (
-    <div className="cr-menu__wrapper">
-      <hr className="cr-menu__divider" />
-      <div className="cr-menu__inner">
-        <div className="cr-menu">
-          <input type="checkbox" id="cr-menu__btn--menu" className="cr-menu__btn" />
-          <label htmlFor="cr-menu__btn--menu" className="cr-menu__label">Menu</label>
-          <input type="checkbox" id="cr-menu__btn--search" className="cr-menu__btn" />
-          <label htmlFor="cr-menu__btn--search" className="cr-menu__label">Search</label>
-          <div className="cr-menu__main-menu">
-            <nav className="cr-menu__nav">
-              <ul role="menubar" tabIndex="0">
-                <li>Main menu</li>
-              </ul>
-            </nav>
+export default class MegaMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.handleNav = this.handleNav.bind(this);
+    this.handleMenuItem = this.handleMenuItem.bind(this);
+  }
+
+  // @TODO: regex to add external class
+  handleMenuItem = (href, title) => {
+    return (
+      <a
+        role='menuitem'
+        href={href}
+      >
+        {title}
+      </a>
+    );
+  };
+
+  // @TODO: top level item weight? and tidy up the rubbish code, mobile onClick reset both checkboxes
+  handleNav = (data) => {
+    const menuItems= Object.keys(data)
+      .filter(obj => data[obj]['#title'] !== undefined )
+      .map(obj => {
+        const level1 = data[obj];
+        const menuBar = (
+          <div className="cr-menu__block">
+            <span />
+            {this.handleMenuItem(level1['#href'], level1['#title'])}
           </div>
-          <div className="cr-menu__search">
-            <form role="search" className="cr-search-input">
-              <label htmlFor="{{ uniqueId }}" className="cr-search-input__label">Search</label>
-              <input
-                type="search"
-                name="search"
-                id="{{ uniqueId }}"
-                className="cr-search-input__input"
-                placeholder="Search..."
-                autoComplete="off"
-                autoCorrect="off"
-              />
-              <button
-                type="submit"
-                className="cr-search-input__button"
-                aria-label="Submit your search"
-              >
-                Search
-              </button>
-            </form>
+        );
+        const menu = Object.keys(level1['#below'])
+          .filter(obj2 => level1['#below'][obj2]['#title'] !== undefined )
+          .map(
+            obj2 => {
+              const level2 = level1['#below'][obj2];
+              const menuItem = Object.keys(level2['#below'])
+                .filter(obj3 => level2['#below'][obj3]['#title'] !== undefined )
+                .map(
+                  obj3 => {
+                    const level3 = level2['#below'][obj3];
+                    return (
+                      <li>
+                        {this.handleMenuItem(level3['#href'], level3['#title'])}
+                      </li>
+                    );
+                  }
+                );
+
+              return (
+                <li>
+                  {this.handleMenuItem(level2['#href'], level2['#title'])}
+                  <ul>
+                    {menuItem}
+                  </ul>
+                </li>
+              );
+            }
+          );
+
+        return (
+          <li>
+            {menuBar}
+            <ul role="menu">
+              {menu}
+            </ul>
+          </li>
+        );
+      });
+
+    return (
+      <nav className="cr-menu__nav">
+        <ul role="menubar" tabIndex="0">
+          {menuItems}
+        </ul>
+      </nav>
+    );
+  };
+
+  render() {
+    return (
+      <Fetch
+        url={this.props.url}
+      >
+        {({ data }) => (
+          <div className="cr-menu__wrapper">
+            <hr className="cr-menu__divider" />
+            <div className="cr-menu__inner">
+              <div className="cr-menu">
+                <input type="checkbox" id="cr-menu__btn--menu" className="cr-menu__btn" />
+                <label htmlFor="cr-menu__btn--menu" className="cr-menu__label">Menu</label>
+                <input type="checkbox" id="cr-menu__btn--search" className="cr-menu__btn" />
+                <label htmlFor="cr-menu__btn--search" className="cr-menu__label">Search</label>
+                <div className="cr-menu__main-menu">
+                  {this.handleNav(data)}
+                </div>
+                <div className="cr-menu__search">
+                  <Search
+                    label='Search'
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
-  );
+        )}
+      </Fetch>
+    );
+  }
 }
 
 MegaMenu.propTypes = {
-  href: PropTypes.string,
-  text: PropTypes.string,
+  url: PropTypes.string,
 };
-
-export default MegaMenu;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Breadcrumb from '../patterns/breadcrumb';
-import Collapsible from '../patterns/collapsible';
+import Collapsible from '../patterns/collapsible.jsx';
 import CtaLink from '../patterns/cta.link';
 import CtaPrimary from '../patterns/cta.primary';
 import CtaSecondary from '../patterns/cta.secondary';
@@ -15,6 +15,7 @@ import ListUnordered from '../patterns/list.unordered';
 import MegaMenu from '../patterns/menu';
 import NavigationBlock from '../patterns/navigation_block';
 import RelatedLinks from '../patterns/related.links';
+import PropTypes from 'prop-types';
 
 // @TODO:
 // fix eslint
@@ -24,28 +25,97 @@ import RelatedLinks from '../patterns/related.links';
 // fix components
 // create readme file
 // bridge between /docs and /lib
+const data = {
+  NavigationBlock: {
+    title: 'Navigation Block',
+    target: '_blank',
+    text: 'Find out about tests to diagnose cancer and monitor it during and after treatment, including what each test can show, how you have it and how to prepare.',
+    href: '#',
+  },
+  RelatedLinks: {
+    title: 'Related links',
+    staticContent: `<ul><li><a href='#'><strong>Side effects of triptorelin</strong></a></li><li><a href='#'><strong>Coping with cancer</strong></a></li><li><a href='#'><strong>Coping with cancer</strong></a></li><li><a href='#'><strong>Your cancer type</strong></a></li></ul>`,
+  },
+  CtaPrimary: {
+    href: '#',
+    text: 'Primary',
+  },
+  CtaSecondary: {
+    href: '#',
+    text: 'Secondary',
+  },
+  CtaLink: {
+    href: '#',
+    text: 'This is text link',
+  },
+  HeaderPageTitle: {
+    text: 'This is H1'
+  },
+  Search: {
+    label: 'Search'
+  },
+  Collapsible: {
+    title: 'Collapsible Box',
+    staticContent: '',
+    content: `<div class="cr-text-pattern"><p>Lobortis viverra torquent cras litora nisl amet tempor et&nbsp; consectetur feugiat bibendum fames ad</p></div><hr class="cr-divider"><h2 class="cr-heading-pattern">Another H2</h2><h3 class="cr-heading-pattern">Another H3</h3><div class="cr-text-pattern"><p>Viverra torquent cras litora nisl amet tempor et consectetur&nbsp; feugiat bibendum fames ad</p></div><h3 class="cr-heading-pattern">Navigation block</h3><a class="cr-cta--primary" href="https://www.cancerresearchuk.org/about-cancer" target="_blank" rel="noopener noreferrer">Primary</a><a class="cr-cta--secondary" href="https://www.cancerresearchuk.org/about-cancer" target="_blank" rel="noopener noreferrer">Secondary</a><hr class="cr-divider">`,
+  },
+  MegaMenu: {
+    url: 'https://uat.cancerresearchuk.org/cruk-navigation/menu-mdd/json'
+  }
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavigationBlock
-          href="#"
-          target="_blank"
-          title="Tests"
-          text="Find out about tests to diagnose cancer and monitor it during and after treatment, including what each test can show, how you have it and how to prepare."
-        />
-        <RelatedLinks
-          title="Related links"
-          staticContent={`<ul><li><a href="#"><strong>Side effects of triptorelin</strong></a></li><li><a href="#"><strong>Coping with cancer</strong></a></li><li><a href="#"><strong>Coping with cancer</strong></a></li><li><a href="#"><strong>Your cancer type</strong></a></li></ul>`}
-        />
-        <HeaderPageTitle />
-        <MegaMenu />
+      <div className='cr-layout'>
+        <div className='cr-layout__row cr-layout__row cr-layout__row--100'>
+          <div className='cr-layout__cell'>
+            <HeaderPageTitle
+              text={data.HeaderPageTitle.text}
+            />
+            <h2 className="cr-heading-pattern">This is H2</h2>
+            <h3 className="cr-heading-pattern">This is H3</h3>
+            <MegaMenu
+              url={data.MegaMenu.url}
+            />
+          </div>
+        </div>
+        <div className='cr-layout__row cr-layout__row cr-layout__row--50-50'>
+          <div className='cr-layout__cell'>
+            <NavigationBlock
+              href={data.NavigationBlock.href}
+              target={data.NavigationBlock.target}
+              title={data.NavigationBlock.title}
+              text={data.NavigationBlock.text}
+            />
+            <Collapsible
+              title={data.Collapsible.title}
+              content={data.Collapsible.content}
+            />
+            <CtaPrimary
+              href={data.CtaPrimary.href}
+              text={data.CtaPrimary.text}
+            />
+            <CtaSecondary
+              href={data.CtaSecondary.href}
+              text={data.CtaSecondary.text}
+            />
+            <CtaLink
+              href={data.CtaLink.href}
+              text={data.CtaLink.text}
+            />
+          </div>
+          <div className='cr-layout__cell'>
+            <RelatedLinks
+              title={data.RelatedLinks.title}
+              staticContent={data.RelatedLinks.staticContent}
+            />
+          </div>
+          <Search
+            label={data.Search.label}
+          />
+        </div>
         <Divider />
-        <CtaLink />
-        <CtaPrimary />
-        <CtaSecondary />
-        <Search />
       </div>
     );
   }
