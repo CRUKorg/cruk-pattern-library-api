3ede9f5948138ff7c59039f17324ced4d8864b42
Jessie Wang <Jessie.Wang@cancer.org.uk>
2019-02-27 16:55:42 +0000

    revert vendor.js

--- docs/app/js/vendor.js
+++ docs/app/js/vendor.js
@@ -5,17 +5,17 @@
  * Handle Mobile menu
  * @TODO: limit to mobile only
  */
-// var handleMobileMenu = function() {
-//   var btnSearch = document.getElementById('cr-menu__btn--search'),
-//     btnMenu = document.getElementById('cr-menu__btn--menu');
-//
-//   function uncheckMenuItemOne() {
-//     document.getElementById('cr-menu__btn--menu').checked = false;
-//   }
-//   function uncheckMenuItemTwo() {
-//     document.getElementById('cr-menu__btn--search').checked = false;
-//   }
-//
-//   btnSearch.addEventListener('click', uncheckMenuItemOne);
-//   btnMenu.addEventListener('click', uncheckMenuItemTwo);
-// }();
+var handleMobileMenu = function() {
+  var btnSearch = document.getElementById('cr-menu__btn--search'),
+    btnMenu = document.getElementById('cr-menu__btn--menu');
+
+  function uncheckMenuItemOne() {
+    document.getElementById('cr-menu__btn--menu').checked = false;
+  }
+  function uncheckMenuItemTwo() {
+    document.getElementById('cr-menu__btn--search').checked = false;
+  }
+
+  btnSearch.addEventListener('click', uncheckMenuItemOne);
+  btnMenu.addEventListener('click', uncheckMenuItemTwo);
+}();
